52592e2c4614be1cc2e1ba30d288bcc4a0d19a1c
Paul Ciarlo <paul.ciarlo@gmail.com>
2018-05-30 08:27:32 -0400

    working on 4bit turbo ecc, will have to compute the tables faster or precompute them. also fix cmake installation

--- crc.c
+++ crc.c
@@ -36,6 +36,8 @@ static uint32_t crc_table[256];
 // correction tables.
 static uint32_t single_bit_syndrome[112];
 
+static uint32_t double_bit_syndrome[112][112];
+
 static void initLookupTables()
 {
     int i;
@@ -56,8 +58,14 @@ static void initLookupTables()
 
     memset(msg, 0, sizeof(msg));
     for (i = 0; i < 112; ++i) {
+        int j;
         msg[i/8] ^= 1 << (7 - (i & 7));
         single_bit_syndrome[i] = modesChecksum(msg, 112);
+        for (j = 0; j < 112; ++j) {
+            msg[j/8] ^= 1 << (7 - (j & 7));
+            double_bit_syndrome[i][j] = modesChecksum(msg, 112);
+            msg[j/8] ^= 1 << (7 - (j & 7));
+        }
         msg[i/8] ^= 1 << (7 - (i & 7));
     }
 }
