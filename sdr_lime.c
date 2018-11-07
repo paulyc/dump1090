71323e75ea472d090778a3b68be56691d2440dba
Paul Ciarlo <paul.ciarlo@gmail.com>
2018-11-07 15:52:38 -0500

    Fix some minor build errors

--- sdr_lime.c
+++ sdr_lime.c
@@ -19,8 +19,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "sdr_lime.h"
 #include "dump1090.h"
+#include "sdr_lime.h"
 
 #include <lime/LimeSuite.h>
 
