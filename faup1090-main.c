5fa64e24e88543ae6935528a59bf184529019681
Paul Ciarlo <paul.ciarlo@gmail.com>
2019-05-08 14:50:18 -0400

    move some things around so merging and rebasing aren't as broken

--- faup1090-main.c
+++ faup1090-main.c
@@ -1,13 +1,11 @@
 // Part of dump1090, a Mode S message decoder for RTLSDR devices.
 //
-// lib1090.h: main library header
-// includes all other dump1090 headers except system headers
-// include this to use lib1090 exports
+// faup1090-main.c: main function for faup1090 executable
 //
 // Copyright (C) 2012 by Salvatore Sanfilippo <antirez@gmail.com>
 // Copyright (c) 2014-2016 Oliver Jowett <oliver@mutability.co.uk>
 // Copyright (c) 2017 FlightAware LLC
-// Copyright (C) 2018-2019 Paul Ciarlo <paul.ciarlo@gmail.com>
+// Copyright (C) 2018 Paul Ciarlo <paul.ciarlo@gmail.com>
 //
 // This file is free software: you may copy, redistribute and/or modify it
 // under the terms of the GNU General Public License as published by the
@@ -27,7 +25,7 @@
 //   Copyright (C) 2012 by Salvatore Sanfilippo <antirez@gmail.com>
 //   Copyright (c) 2014-2016 Oliver Jowett <oliver@mutability.co.uk>
 //   Copyright (c) 2017 FlightAware LLC
-//   Copyright (C) 2018-2019 Paul Ciarlo <paul.ciarlo@gmail.com>
+//   Copyright (C) 2018 Paul Ciarlo <paul.ciarlo@gmail.com>
 //
 //   All rights reserved.
 //
@@ -54,57 +52,8 @@
 //   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 //   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef DUMP1090_LIB1090_H
-#define DUMP1090_LIB1090_H
-
 #include "dump1090.h"
 
-// from lib1090.c
-static const double _3dBFS = 0.501187234; //pow(10.0, -3.0/10.0);
-
-struct lib1090Config_t {
-    const char *userLat;
-    const char *userLon;
-    const char *userAltMeters;
-    int pipedes[2];
-    const char *beastOutPipeName;
-    int pipefd;
-    pid_t childPid;
-    double sample_rate;
-    const char *jsonDir;
-};
-
-void lib1090GetConfig(struct lib1090Config_t **configOut);
-void lib1090GetModes(struct modes_t** modesOut);
-
-int lib1090Init();
-int lib1090Uninit();
-
-int lib1090RunThread(void *udata);
-int lib1090JoinThread(void **retptr);
-
-ssize_t lib1090HandleFrame(struct modesMessage *mm, uint8_t *frm, uint64_t timestamp);
-int lib1090FixupFrame(uint8_t *frameIn, uint8_t *frameOut); // check crc, fix if possible
-ssize_t lib1090DecodeFrame(struct modesMessage *mm, uint8_t *frame, uint64_t timestamp, double signalLevel);
-ssize_t lib1090FormatBeast(struct modesMessage *mm, uint8_t *beastBufferOut, size_t beastBufferLen, bool writeToPipe);
-
-struct dump1090Fork_t {
-    const char *userLat;
-    const char *userLon;
-    int pipedes[2];
-    pid_t childPid;
-    double sample_rate;
-    const char *jsonDir;
-    char scratch[128];
-};
-
-int lib1090InitDump1090(struct dump1090Fork_t **forkInfoOut);
-int lib1090ForkDump1090(struct dump1090Fork_t *forkInfo);
-int lib1090KillDump1090(struct dump1090Fork_t *forkInfo);
-int lib1090FreeDump1090(struct dump1090Fork_t **pForkInfo);
-
-#ifdef __cplusplus
+int main(int argc, char **argv) {
+    return faup1090main(argc, argv);
 }
-#endif
-
-#endif // DUMP1090_LIB1090_H
